290b06fbd2fc8e75072e7d39efa6c5395542b854
manasvishrivastava09 <manasvishrivastava2000@gmail.com>
2024-02-23 00:39:10 +0530

    food delivery app creation started

--- App.js
+++ App.js
@@ -1,36 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+/**
+ * Header
+ *  -Loop
+ *  -Nav Items
+ * Body
+ *  -Search
+ *  -RestaurantContainer
+ *      -RestaurantsCard
+ * Footer
+ *  -Copyright
+ *      -Links
+ *      -Address
+ *  -Contact
+ * 
+ */
 
-const Title = ()=>(
-    <h1 className="head" tabIndex="5">
-    Namaste React using JSX
-    </h1>
-    );
+const Header =()=>{
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img 
+                className="logo" 
+                src="https://img.freepik.com/premium-vector/man-is-riding-scooter-delivery-logo-vector_396343-758.jpg?size=626&ext=jpg&uid=R46564419&ga=GA1.1.198370958.1708610606&semt=ais"
+                />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>🏠Home</li>
+                    <li>📰About Us</li>
+                    <li>📪Contact Us</li>
+                    <li>🛒Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+};
 
-    const elem =<span>React Elem</span>;
+const Body = () => {
+    return(
+        <div>Body</div>
+    )
+};
+const AppLayout = () => {
+    return (
+        <div className="app">
+          <Header/>  
+          <Body/>
+        </div>
+    )
+       
 
-    const ElementReact=(
-        <h1 className="head" tabIndex="5">
-            {elem}
-        Namaste Element
-        </h1>
-    );
-    const test=100;
+    
+}
 
-    //Component Composition
-
-    const HeadingComponent=()=>(
-    <div id="container">
-        <Title/>
-        <h2>{test}</h2>
-        <h2>{ElementReact}</h2>
-        <h1>Namaste React Functional component</h1>
-    </div>
-);
 
 const root = ReactDOM.createRoot
 (document.getElementById("root"));
 
-root.render(<HeadingComponent/>);
+root.render(<AppLayout/>);
 
