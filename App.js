78cba3ba89fadc2a3f053a04dbd8fda5ce285ceb
manasvishrivastava09 <manasvishrivastava2000@gmail.com>
2024-02-22 15:08:15 +0530

    javascript inside jsx {}

--- App.js
+++ App.js
@@ -8,12 +8,25 @@ const Title = ()=>(
     </h1>
     );
 
-const HeadingComponent=()=>(
+    const elem =<span>React Elem</span>;
+
+    const ElementReact=(
+        <h1 className="head" tabIndex="5">
+            {elem}
+        Namaste Element
+        </h1>
+    );
+    const test=100;
+
+    //Component Composition
+
+    const HeadingComponent=()=>(
     <div id="container">
         <Title/>
+        <h2>{test}</h2>
+        <h2>{ElementReact}</h2>
         <h1>Namaste React Functional component</h1>
     </div>
-
 );
 
 const root = ReactDOM.createRoot
